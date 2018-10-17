ba5ad5cedbd7180b61fcaabf7d47c4215df7e79a
Dominic Wroblewski <dominic.wroblewski@sky.uk>
2018-10-17 11:54:35 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,30 @@
+# MapleStory 2 Boss Timer
+
+World Boss timer for MapleStory 2 (NA/EU).
+
+## GitHub Pages & Jekyll
+
+The GitHub pages are updated automatically when deploying to `master`. This uses Jekyll to build the site after any changes.
+
+Requirements:
+
+- [Ruby 2.5.1](https://www.ruby-lang.org/en/downloads/)
+- [Jekyll](https://jekyllrb.com/)
+
+### Setup
+
+```bash
+gem install bundler
+bundle install
+jekyll serve --livereload
+```
+
+This will then run on `localhost:4000` and automatically reload when you change any files.
+
+Updating of the site will happen when a push is made to the `master` branch.
+
+## Pull Requests
+
+I'm more than happy for anyone to help out by submitting a pull request! Feel free to fork the repo and make any changes to submit.
+
+If you have any feature requests or have found any bugs/issues, feel free to submit an Issue on the main repo.
---
+++ assets/js/timer.js
@@ -0,0 +1,35 @@
+function BossTimer() {
+
+  console.log('test');
+
+  var self = this;
+
+  self.events = {
+    "Acreon": { minutes: 0, map: "" },
+    "Alpha Turtle": { minutes: 0, map: "" },
+    "Amadon": { minutes: 0, map: "" },
+    "Bajar": { minutes: 0, map: "" },
+    "Cold-Blooded Baphomet": { minutes: 0, map: "" },
+    "Devilin Warrior": { minutes: 0, map: "" },
+    "Dun Dun": { minutes: 0, map: "" },
+    "Furious Baphomet": { minutes: 0, map: "" },
+    "Giant Turtle": { minutes: 0, map: "" },
+    "Griffin": { minutes: 0, map: "" },
+    "Griffina": { minutes: 0, map: "" },
+    "Heartless Baphomet": { minutes: 0, map: "" },
+    "Ica Le Madrid": { minutes: 0, map: "" },
+    "Ikar Morde": { minutes: 0, map: "" },
+    "Lernos": { minutes: 0, map: "" },
+    "Lulu and Momos": { minutes: 0, map: "" },
+    "Mark 52 Alpha Bot": { minutes: 0, map: "" },
+    "Pekanos": { minutes: 0, map: "" },
+    "Toh and Googoo": { minutes: 0, map: "" },
+    "Toto and Waggus": { minutes: 0, map: "" },
+    "Ureus": { minutes: 0, map: "" },
+    "Vayar Gatekeeper": { minutes: 0, map: "" }
+  }
+}
+
+$(document).ready(function() {
+  console.log('test 2');
+});
